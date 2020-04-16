c00cc121070c3a98dc7b15a141c1e8c7875bde1b
LILKAM <kamil.lilla85@gmail.com>
2020-04-17 00:07:54 +0200

    Okodowanie API i testy z Postmanem

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Controllers
 {
@@ -19,37 +21,86 @@ namespace PortalRandkowy.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext context;
+
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        //public ActionResult<IEnumerable<string>> Get()
+        // Zmieniamy na IActionResult, bo zwracamy ok, a nie listę
+        public IActionResult GetValues()
         {
-                // throw new Exception("Generujemy wyjątek");
-            return new string[] { "value1", "value2" };
+            // throw new Exception("Generujemy wyjątek");
+            //return new string[] { "value1", "value2" };
+            // zwracamy wszystkie wartości
+            var values = this.context.Values.ToList();
+            // zwracamy status OK wraz ze wszystkimi wartościami
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        //public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            // zwracamy wartość po id, dlatego jest x.id, jeśli inaczej to trzeba zmienić x.id
+            // Można zamiast First wybrać FirstOrDefault. Metoda First zwróci wyjątek jeśli nie znajdzie wartości. FirstOrDefault nie. Nie wiem tylko co zwróci jak nie znajdzie? NULL?
+            // FirstOrDefault jednak zwróci "204 no content", Find zwraca też 204 no content, First zwróci "500 Internal Server Error" czyli niejasny komunikat.
+            var value = this.context.Values.FirstOrDefault(x => x.id == id);
+            // zwracamy status OK wraz ze wszystkimi wartościami
+            return Ok(value);
         }
 
         // POST api/values
+        // Służy do dodawania danych
         [HttpPost]
-        public void Post([FromBody] string value)
+        //public void AddValue([FromBody] Value value)
+        // Zamiast void dajemy sobie IActionResult, żeby móc zrobić status OK
+        public IActionResult AddValue([FromBody] Value value)
         {
+            this.context.Values.Add(value);
+            // Metoda, która zapisze nasze zmiany do bazy danych
+            this.context.SaveChanges();
+            return Ok(value);
         }
 
         // PUT api/values/5
+        // Służy do edycji danych
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        //public void EditValue(int id, [FromBody] Value value)
+        // Zamiast void dajemy sobie IActionResult, żeby móc zrobić status OK
+        public IActionResult EditValue(int id, [FromBody] Value value)
         {
+            // Znajdujemy najpierw wartość o podanym id. Nie wiem czy metody find nie lepiej użyć powyżej dla GetValue? Zwróci nam prawidłowo nulla przecież, a nie wartość domyślną.
+            // Nie lepiej, bo // FirstOrDefault jednak zwróci "204 no content", Find zwraca też 204 no content, First zwróci "500 Internal Server Error" czyli niejasny komunikat.
+            var data = this.context.Values.Find(id);
+            //var oldData = data;
+            data.Name = value.Name;
+            this.context.Update(data);
+            this.context.SaveChanges();
+            //return Ok("OldData: " + oldData + " NewData: " + data); <-- tak nie zadziała niestety, bo zwracać musi json, wypisze "data.value" zamiast json
+            return Ok(data);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        //public void DeleteValue(int id)
+        // Zamiast void dajemy sobie IActionResult, żeby móc zrobić status OK
+        public IActionResult DeleteValue(int id)
         {
+            var data = this.context.Values.Find(id);
+            //var oldData = data;
+            // Zabezpieczenie przed zwróceniem Internal server error. Zwróci nam dzięki temu 204 No Content.
+            if (data == null)
+                return NoContent();
+
+            this.context.Remove(data);
+            this.context.SaveChanges();
+            //return Ok("OldData: " + oldData + " NewData: " + data); <-- tak nie zadziała niestety, bo zwracać musi json, wypisze "data.value" zamiast json
+            return Ok(data);
         }
     }
 }
