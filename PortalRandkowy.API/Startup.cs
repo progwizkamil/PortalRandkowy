be022a361f57ba081d42bb8d5d561cc486e69f18
LILKAM <kamil.lilla85@gmail.com>
2020-04-16 19:45:57 +0200

    Po przejściu z .net core 2.1 na 2.2

--- PortalRandkowy.API/Startup.cs
+++ PortalRandkowy.API/Startup.cs
@@ -6,12 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using PortalRandkowy.API.Data;
-using Microsoft.EntityFrameworkCore;
+
 
 namespace PortalRandkowy.API
 {
